f45a8b39d9154b354262e1a2986cc7171b96ba71
Gustavo Silva <gustavohenriquesilva124@gmail.com>
2022-03-14 11:45:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+function App() {
+  return (
+    <section>
+      <FormularioCadastro/>
+      <ListaDeNotas/>
+    </section>
+  );
+}
+
+export default App;
