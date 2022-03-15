9a1eda3a7cf29f68b3a8c0301778fa14e208ef5c
Gustavo Silva <gustavohenriquesilva124@gmail.com>
2022-03-15 16:04:45 -0300

    Modulo - Lidando com Eventos e propiedades

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,18 +1,41 @@
 import React, { Component } from "react";
 import "./style.css";
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  _handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+    console.log(this.titulo);
+  }
+
+  _handleMudancaTexto(evento) {
+    this.texto = evento.target.value;
+    console.log(this.titulo)
+  }
+  _criarNota(evento){
+    evento.preventDefault()
+    console.log(`Nova nota criada ${this.texto} ${this.titulo}`);
+  }
   render() {
     return (
-      <form className="form-cadastro ">
+      <form className="form-cadastro "
+      onSubmit={this._criarNota.bind(this)} 
+      >
         <input
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this._handleMudancaTexto.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
